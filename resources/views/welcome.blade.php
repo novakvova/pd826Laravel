c3a7bae1b8199facf928315cf239868886230978
novakvova <novakvova@gmail.com>
2020-05-28 19:16:21 +0300

    Update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Привіт друзі
                 </div>
 
                 <div class="links">
